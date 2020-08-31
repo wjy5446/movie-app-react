cdf4470082f0146340fd06203ca99a89629206d6
wjy5446 <wjy5446@gmail.com>
2020-09-01 00:12:59 +0900

    feat: add route

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+      About this page: I built it because I love movie
+    </div>
+  );
+}
+
+export default About;
